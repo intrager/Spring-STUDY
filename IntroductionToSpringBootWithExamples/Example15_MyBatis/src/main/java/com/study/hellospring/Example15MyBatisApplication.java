5416a7f5e85bdd77d807f98b7d9a1739a29a6656
Bruce Han <intrager@naver.com>
2022-05-09 23:35:45 +0900

    Basic of MyBatis

---
+++ IntroductionToSpringBootWithExamples/Example15_MyBatis/src/main/java/com/study/hellospring/Example15MyBatisApplication.java
@@ -0,0 +1,13 @@
+package com.study.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example15MyBatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example15MyBatisApplication.class, args);
+	}
+
+}
