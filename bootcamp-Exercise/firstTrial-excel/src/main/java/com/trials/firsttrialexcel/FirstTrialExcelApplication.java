47369fbd847339fdcb425b44d3ee29dfe57d09ee
Bruce Han <intrager@naver.com>
2022-05-15 23:31:14 +0900

    assignment with excel - fixing render error

---
+++ bootcamp-Exercise/firstTrial-excel/src/main/java/com/trials/firsttrialexcel/FirstTrialExcelApplication.java
@@ -0,0 +1,13 @@
+package com.trials.firsttrialexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTrialExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTrialExcelApplication.class, args);
+    }
+
+}
