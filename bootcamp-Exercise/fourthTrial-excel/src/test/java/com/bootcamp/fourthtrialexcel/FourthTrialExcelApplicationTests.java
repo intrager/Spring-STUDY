82804548dc74ea1c4c73ba8258d4c8f8ae8ef0f4
Bruce Han <intrager@naver.com>
2022-05-18 23:58:43 +0900

    Excel Trial

---
+++ bootcamp-Exercise/fourthTrial-excel/src/test/java/com/bootcamp/fourthtrialexcel/FourthTrialExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.fourthtrialexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthTrialExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
