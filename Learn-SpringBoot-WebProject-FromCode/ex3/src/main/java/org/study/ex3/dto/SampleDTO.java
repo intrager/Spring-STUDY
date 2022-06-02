d259a5df651fa42da7f7577f436d8cf16bfea1dd
Bruce Han <intrager@naver.com>
2022-06-02 23:53:09 +0900

    Spring MVC and Thymeleaf Practice

---
+++ Learn-SpringBoot-WebProject-FromCode/ex3/src/main/java/org/study/ex3/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package org.study.ex3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data // Getter/Setter, toString(), equals(), hashCode()를 자동으로 생성
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
