83808539d2fa7aa7ae181a6c7ed9ad0fb0ffee96
Bruce Han <intrager@naver.com>
2022-06-07 23:43:34 +0900

    N : 1 - many-to-one associations Practice2

--- Learn-SpringBoot-WebProject-FromCode/board/src/main/java/org/study/board/repository/search/SearchBoardRepository.java
+++ Learn-SpringBoot-WebProject-FromCode/board/src/main/java/org/study/board/repository/search/SearchBoardRepository.java
@@ -1,7 +1,11 @@
 package org.study.board.repository.search;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.study.board.entity.Board;
 
 public interface SearchBoardRepository {
     Board search1();
+
+    Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
 }
--- Learn-SpringBoot-WebProject-FromCode/board/src/test/java/org/study/board/repository/BoardRepositoryTests.java
+++ Learn-SpringBoot-WebProject-FromCode/board/src/test/java/org/study/board/repository/BoardRepositoryTests.java
@@ -96,4 +96,10 @@ public class BoardRepositoryTests {
     public void testSearch1() {
         boardRepository.search1();
     }
+
+    @Test
+    public void testSearchPage() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.searchPage("t", "1", pageable);
+    }
 }
