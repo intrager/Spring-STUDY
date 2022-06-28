9d5c27640ca0173851c4b99bb6659a5742bd7677
Bruce Han <intrager@naver.com>
2022-06-29 00:49:29 +0900

    Return of upload result and screen processing - Controller

--- Learn-SpringBoot-WebProject-FromCode/moviereview/src/main/java/org/review/moviereview/controller/UploadController.java
+++ Learn-SpringBoot-WebProject-FromCode/moviereview/src/main/java/org/review/moviereview/controller/UploadController.java
@@ -4,14 +4,19 @@ package org.review.moviereview.controller;
 import lombok.extern.log4j.Log4j2;
 import org.review.moviereview.dto.UploadResultDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -65,6 +70,32 @@ public class UploadController {
             }
         }   // end for
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName : " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file : " + file);
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+
+            // MIME 타입 처리
+            httpHeaders.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), httpHeaders, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 
     private String makeFolder() {
