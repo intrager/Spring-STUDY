1a3b057d9a3e59bad7bce153b38fb5d33db44b96
Bruce Han <intrager@naver.com>
2022-07-02 00:17:10 +0900

    Using Query Results in MyBatis

--- IntroductionToSpringBootWithExamples/Example18_MyBatis_ResultNum/src/main/java/com/study/hellospring/MyController.java
+++ IntroductionToSpringBootWithExamples/Example18_MyBatis_ResultNum/src/main/java/com/study/hellospring/MyController.java
@@ -1,7 +1,13 @@
 package com.study.hellospring;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.study.hellospring.dao.ISimpleBbsDAO;
@@ -17,6 +23,52 @@ public class MyController {
 		return "redirect:list";
 	}
 	
+	@RequestMapping("/list")
+	public String userListPage(Model model) {
+		model.addAttribute("list", iSimpleBbsDao.listDAO());
+		
+		int nTotalCount = iSimpleBbsDao.articleCount();
+		System.out.println("Count : " + nTotalCount);
+		
+		return "/list";
+	}
 	
+	@RequestMapping("/view")
+	public String view(HttpServletRequest request, Model model) {
+		String sId = request.getParameter("id");
+		model.addAttribute("dto", iSimpleBbsDao.viewDAO(sId));
+		return "/view";
+	}
+	
+	@RequestMapping("/writeForm")
+	public String writeForm() {
+		return "/writeForm";
+	}
+	
+	@RequestMapping("/write")
+	public String write(HttpServletRequest request, Model model) {
+		String sName = request.getParameter("writer");
+		String sTitle = request.getParameter("title");
+		String sContent = request.getParameter("content");
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("item1", sName);
+		map.put("item2", sTitle);
+		map.put("item3", sContent);
+		
+		int nResult = iSimpleBbsDao.writeDAO(map);
+		System.out.println("Write : " + nResult);
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(HttpServletRequest request, Model model) {
+		String sId = request.getParameter("id");
+		int nResult = iSimpleBbsDao.deleteDAO(sId);
+		System.out.println("Delete : " + nResult);
+		
+		return "redirect:list";
+	}
 	
 }
--- IntroductionToSpringBootWithExamples/Example18_MyBatis_ResultNum/src/main/java/com/study/hellospring/dao/ISimpleBbsDAO.java
+++ IntroductionToSpringBootWithExamples/Example18_MyBatis_ResultNum/src/main/java/com/study/hellospring/dao/ISimpleBbsDAO.java
@@ -3,12 +3,17 @@ package com.study.hellospring.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.study.hellospring.dto.SimpleBbsDTO;
+
+@Mapper
 public interface ISimpleBbsDAO {
-	public List<SimpleBbsDTO> listDao();
-	public SimpleBbsDTO viewDao(String id);
-	public int writeDao(Map<String, String> map);
-	public int deleteDao(@Param("_id") String id);
+	
+	public List<SimpleBbsDTO> listDAO();
+	public SimpleBbsDTO viewDAO(String id);
+	public int writeDAO(Map<String, String> map);
+	public int deleteDAO(@Param("_id") String id);
 	public int articleCount();
 }
