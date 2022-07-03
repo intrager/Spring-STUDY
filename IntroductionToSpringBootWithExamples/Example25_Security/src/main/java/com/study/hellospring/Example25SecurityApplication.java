c794f0962b29b29e8f1ecc644575fc8c4352e2b5
Bruce Han <intrager@naver.com>
2022-07-03 16:27:21 +0900

    Security Basics Practice

---
+++ IntroductionToSpringBootWithExamples/Example25_Security/src/main/java/com/study/hellospring/Example25SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.study.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example25SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example25SecurityApplication.class, args);
+	}
+
+}
