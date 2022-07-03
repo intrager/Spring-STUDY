72e338c7fa3ecb37cb535fae6c37ee4676a302b8
Bruce Han <intrager@naver.com>
2022-07-04 02:05:54 +0900

    Practice - Using an external library

---
+++ IntroductionToSpringBootWithExamples/Example31_FileUpload/src/main/java/com/study/hellospring/MyController.java
@@ -0,0 +1,63 @@
+package com.study.hellospring;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception {
+		return "FileUpload";
+	}
+	
+	@RequestMapping("/uploadForm")
+	public String uploadForm() {
+		return "fileForm";
+	}
+	
+	@RequestMapping("/uploadOk")
+	public @ResponseBody String uploadOk(HttpServletRequest request) {
+		int size = 1024 * 1024 * 10;	// 10MB
+		String file = "";
+		String oriFile = "";
+		
+		JSONObject obj = new JSONObject();
+		
+		try {
+			String path = ResourceUtils
+					.getFile("classpath:static/upload/").toPath().toString();
+			// System.out.println(path);
+			
+			MultipartRequest multi = new MultipartRequest(request, path, size, 
+														"UTF-8", new DefaultFileRenamePolicy());
+		
+			System.out.println("1111111");
+			Enumeration files = multi.getFileNames();
+			String str = (String)files.nextElement();
+			
+			file = multi.getFilesystemName(str);
+			oriFile = multi.getOriginalFileName(str);
+			
+			System.out.println(file);
+			System.out.println(oriFile);
+			obj.put("success", 1);
+			obj.put("desc", "업로드 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+			obj.put("success", 0);
+			obj.put("desc",	"업로드 실패");
+		}
+		
+		return obj.toJSONString();
+	}
+}
