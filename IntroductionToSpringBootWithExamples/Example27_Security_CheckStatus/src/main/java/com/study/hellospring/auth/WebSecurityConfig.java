2e20093c735f5a2405c2524f073b5fb6477e1797
Bruce Han <intrager@naver.com>
2022-07-03 20:09:48 +0900

    Practice - Check Security Status

--- IntroductionToSpringBootWithExamples/Example27_Security_CheckStatus/src/main/java/com/study/hellospring/auth/WebSecurityConfig.java
+++ IntroductionToSpringBootWithExamples/Example27_Security_CheckStatus/src/main/java/com/study/hellospring/auth/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.study.hellospring.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,11 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	public AuthenticationFailureHandler authenticationFailureHandler;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,26 +28,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated();
 		
 		http.formLogin()
-				.loginPage("/loginForm")			// default : login
+				.loginPage("/loginForm")
 				.loginProcessingUrl("/j_spring_security_check")
-				.failureUrl("/loginError")			// default : /login?error
-				.usernameParameter("j_username")	// 지정한 변수명으로 파라미터명 지정
-				.passwordParameter("j_password")	// 지정한 변수명으로 파라미터명 지정
+				.failureHandler(authenticationFailureHandler)
+				.usernameParameter("j_username")
+				.passwordParameter("j_password")
 				.permitAll();
 		
 		http.logout()
-				.logoutUrl("/logout")	// default
+				.logoutUrl("/logout")
 				.logoutSuccessUrl("/")
 				.permitAll();
 		
-		/**
-		 *  ssl을 사용하지 않으면 true로 사용
-		 *  @EnableWebSecurity 어노테이션에 의해 CSRF 프로덕션이 활성화됨.
-		 *  사이트 간 요청 위조(CSRF : Cross Site Request Forgery)는 
-		 *  개발 중에는 불편할 수 있는 기능이므로
-		 *  http에서 https에 대한 요청을 제출하는 등의 상황에서 잘못된 토큰 예외(403)를 발생시킴.
-		 *  그러므로, 밑에 있는 줄처럼 설정 꺼놓는 게 좋음
-		 */
 		http.csrf().disable();
 	}
 	
