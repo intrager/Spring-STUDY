653ddd91a020912f34c600c37cbe3f21607b2a55
Bruce Han <intrager@naver.com>
2022-07-06 01:36:20 +0900

    Customizing Spring Security

--- Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/example/auth/config/SecurityConfig.java
+++ Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -1,10 +1,44 @@
 package com.example.auth.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        /*
+            HttpSecurity -> formLogin()
+            인가/인증 절차에서 문제가 발생했을 때 로그인 페이지를 보여주도록 지정할 수 있고,
+            화면으로 로그인 방식을 지원한다는 의미로 사용됨.
+         */
+        http.formLogin();   // 인가, 인증에 문제 시 로그인 화면
+        http.csrf().disable();
+        http.logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+            .password("$2a$10$0Lq.XKfrqxIPoLxFm0Zxz.ZpXUg9zHsECEYPRAW9F1xjePS6Y.lu2")
+            .roles("USER");
+    }
 }
---
+++ Learn-SpringBoot-WebProject-FromCode/auth/src/test/java/com/example/auth/security/PasswordTests.java
@@ -0,0 +1,33 @@
+package com.example.auth.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTests {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testEncode() {
+        String password = "1q2w3e4r";
+        String enPw = passwordEncoder.encode(password);
+
+        System.out.println("enPw : " + enPw);
+
+        boolean matchResult = passwordEncoder.matches(password, enPw);
+
+        System.out.println("matchResult: " + matchResult);
+    }
+}
+/*
+첫 번째 실행
+enPw : $2a$10$qattGM7T.QuhifGVg6.1JeAI92yee53p0pCnDRsfvJo12QhRlEcEO
+matchResult: true
+
+두 번째 실행
+enPw : $2a$10$0Lq.XKfrqxIPoLxFm0Zxz.ZpXUg9zHsECEYPRAW9F1xjePS6Y.lu2
+matchResult: true
+ */
