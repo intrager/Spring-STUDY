7762364dc60237d7c31db0ba618fb63bd3b18509
Bruce Han <intrager@naver.com>
2022-07-07 01:04:54 +0900

    Printing authenticated information in Thymeleaf / Controller

--- Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/study/auth/controller/SampleController.java
+++ Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/study/auth/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.study.auth.controller;
 
+import com.study.auth.security.dto.AuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,11 @@ public class SampleController {
     }
 
     @GetMapping("/member")
-    public void exMember() {
+    public void exMember(@AuthenticationPrincipal AuthMemberDTO authMemberDTO) {
         log.info("exMember----------");
+
+        log.info("-------------------------");
+        log.info(authMemberDTO);    // 7월7일 00:55 "오 이렇게 로그를 찍어볼 수 있겠구나"
     }
 
     @GetMapping("/admin")
