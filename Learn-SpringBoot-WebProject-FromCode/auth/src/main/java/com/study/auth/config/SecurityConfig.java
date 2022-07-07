756f682a8e0dcd1c9bb3bf00915f27ef50cd4512
Bruce Han <intrager@naver.com>
2022-07-07 19:44:27 +0900

    Implement 'Remember me' and @PreAuthorize

--- Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/study/auth/config/SecurityConfig.java
+++ Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/study/auth/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.study.auth.config;
 
 import com.study.auth.security.handler.AuthLoginSuccessHandler;
+import com.study.auth.security.service.AuthUserDetailsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +13,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthUserDetailsService authUserDetailsService;
+
+    public SecurityConfig(AuthUserDetailsService authUserDetailsService) {
+        this.authUserDetailsService = authUserDetailsService;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -20,9 +29,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
         /*
             HttpSecurity -> formLogin()
             인가/인증 절차에서 문제가 발생했을 때 로그인 페이지를 보여주도록 지정할 수 있고,
@@ -33,6 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout();
 
         http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60 * 60 * 24 * 7)    // 소셜 로그인은 remember-me를 사용할 수 없음
+                .userDetailsService(authUserDetailsService);    // 7 days
     }
 
     @Bean
--- Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/study/auth/controller/SampleController.java
+++ Learn-SpringBoot-WebProject-FromCode/auth/src/main/java/com/study/auth/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.study.auth.controller;
 
 import com.study.auth.security.dto.AuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j2
 @RequestMapping("/sample/")
 public class SampleController {
+
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll() {
         log.info("exAll-------------");
@@ -24,8 +27,18 @@ public class SampleController {
         log.info(authMemberDTO);    // 7월7일 00:55 "오 이렇게 로그를 찍어볼 수 있겠구나"
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin() {
         log.info("exAdmin-----------");
+    }
+
+    @PreAuthorize("#authMemberDTO != null && #authMemberDTO.username eq \"user100@study.org\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal AuthMemberDTO authMemberDTO) {
+        log.info("exMemberOnly");
+        log.info(authMemberDTO);
+
+        return "/sample/admin";
     }
 }
