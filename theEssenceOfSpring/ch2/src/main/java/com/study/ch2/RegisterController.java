ff3ebf3ee8fbe4614e6673b47683e196fdcb2fbe
Bruce Han <intrager@naver.com>
2022-07-17 21:45:29 +0900

    Learn and Practice abour Request & Forward

--- theEssenceOfSpring/ch2/src/main/java/com/study/ch2/RegisterController.java
+++ theEssenceOfSpring/ch2/src/main/java/com/study/ch2/RegisterController.java
@@ -12,11 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class RegisterController {
 	
-//	@RequestMapping(value = "/register/add", method = {RequestMethod.GET, RequestMethod.POST})	// RequestMapping에 method를 안 적으면 GET, POST 둘 다 허용
-//	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm";	// WEB-INF/views/registerForm.jsp
-//	}
+	@RequestMapping(value = "/register/add", method = {RequestMethod.GET, RequestMethod.POST})	// RequestMapping에 method를 안 적으면 GET, POST 둘 다 허용
+	public String register() {
+		return "registerForm";	// WEB-INF/views/registerForm.jsp
+	}
 
 //	@RequestMapping(value = "/register/save", method = RequestMethod.POST)
 	@PostMapping("/register/save")	// Spring 4.3버전부터 사용 가능
@@ -26,7 +25,7 @@ public class RegisterController {
 			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
 			
 			model.addAttribute("msg", msg);
-			return "redirect:/register/add";
+			return "forward:/register/add";
 //			return "redirect:/register/add?msg=" + msg;	// URL 재작성(rewriting)
 		}
 		// 2. DB에 신규회원 정보를 저장
@@ -34,6 +33,6 @@ public class RegisterController {
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 }
