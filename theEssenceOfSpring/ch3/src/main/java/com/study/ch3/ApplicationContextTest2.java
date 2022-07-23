285b8910f5b15fa60323bc7ebc0feada65a2aa66
Bruce Han <intrager@naver.com>
2022-07-23 21:55:21 +0900

    Learn and Practice about @Resource, @Component, @Value, @PropertySource & Compare Spring Annotation and Standard Annotation(JSR-330) & Straighten out <property> and setter / <constructor-arg> and constructor / <list><set><map>

--- theEssenceOfSpring/ch3/src/main/java/com/study/ch3/ApplicationContextTest2.java
+++ theEssenceOfSpring/ch3/src/main/java/com/study/ch3/ApplicationContextTest2.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -48,11 +49,40 @@ class Car {
 	
 }
 
+@Component
+@PropertySource("setting.properties")
+class SysInfo {
+	@Value("#{systemProperties['user.timezone']}")
+	String timeZone;
+	@Value("#{systemEnvironment['APPDATA']}")
+	String currDir;
+	@Value("${autosaveDir}")
+	String autosaveDir;
+	@Value("${autosaveInterval}")
+	int autosaveInterval;
+	@Value("${autosave}")
+	boolean autosave;
+	@Override
+	public String toString() {
+		return "SysInfo [timeZone=" + timeZone + ", currDir=" + currDir + ", autosaveDir=" + autosaveDir
+				+ ", autosaveInterval=" + autosaveInterval + ", autosave=" + autosave + "]";
+	}
+	
+	
+}
+
 public class ApplicationContextTest2 {
 	public static void main(String[] args) {
 		ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
 //		Car car = ac.getBean("car", Car.class);	// 타입을 지정하면 형변환 안 해도 됨. 아래 문장과 동일함
 		Car car = (Car) ac.getBean("car");	// 이름으로 빈 검색
 		System.out.println("car = " + car);
-}
+		
+		System.out.println("ac.getBean(SysInfo.class) = " + ac.getBean(SysInfo.class));
+		Map<String, String> map = System.getenv();
+		System.out.println("map = " + map);
+		
+		Properties properties = System.getProperties();
+		System.out.println("properties = " + properties);
+	}
 }
