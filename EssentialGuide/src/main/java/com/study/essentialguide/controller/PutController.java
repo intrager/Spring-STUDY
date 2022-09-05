7abf449dde0bdc2cc703cc07343f153fccfa3879
Bruce Han <intrager@naver.com>
2022-09-06 00:23:02 +0900

    Learn aboue PUT API and practice those example

---
+++ EssentialGuide/src/main/java/com/study/essentialguide/controller/PutController.java
@@ -0,0 +1,47 @@
+package com.study.essentialguide.controller;
+
+import com.study.essentialguide.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    // @RequestBody와 Map을 활용한 PUT 메서드 구현
+    @PutMapping(value = "/member")
+    public String putMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    // DTO 객체를 활용한 PUT 메서드 구현
+    // http://localhost:8081/api/v1/put-api/member1
+    @PutMapping(value = "/member1")
+    public String putMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+    // http://localhost:8081/api/v1/put-api/member2
+    @PutMapping(value = "/member2")
+    public MemberDto putMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+    // ResponseEntity를 활용한 PUT 메서드 구현
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> putMemberDto3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+}
