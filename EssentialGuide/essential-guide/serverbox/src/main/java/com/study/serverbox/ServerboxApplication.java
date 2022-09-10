557aeb8e540ff1f2c1c3633c55451c06bb53bc30
Bruce Han <intrager@naver.com>
2022-09-10 20:54:58 +0900

    Learning using actuators & practicing communication between servers

---
+++ EssentialGuide/essential-guide/serverbox/src/main/java/com/study/serverbox/ServerboxApplication.java
@@ -0,0 +1,13 @@
+package com.study.serverbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerboxApplication.class, args);
+    }
+
+}
